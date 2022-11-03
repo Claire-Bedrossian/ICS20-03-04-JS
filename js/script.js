8deca649b7cc9f19eeaf2025fabf659c087f7940
Claire-Bedrossian <113055648+Claire-Bedrossian@users.noreply.github.com>
2022-11-03 14:19:29 -0400

    Added functionality

--- js/script.js
+++ js/script.js
@@ -1,23 +1,31 @@
-// Copyright (c) 202X Claire Bedrossian All rights reserved
+// Copyright (c) 2022 Claire Bedrossian All rights reserved
 //
 // Created by: Claire Bedrossian
-// Created on: XXX 202X
+// Created on: Nov 2022
 // This file contains the JS functions for index.html
 
 "use strict"
 
 /**
- * Check servie worker.
+ * Check service worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/ICS20-03-04-JS/sw.js", {
+    scope: "/ICS20-03-04-JS/",
   })
 }
 
 /**
- * This function displays an alert.
+ * This function converts degrees fahrenheit to degrees celsius.
  */
 function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+    // input
+    const fahrenheit = parseFloat(document.getElementById("fahrenheit-number").value)
+
+    //process
+    const celsius = (fahrenheit - 32) * 5 / 9
+  
+    // output
+    document.getElementById("degrees-celsius").innerHTML =
+      "Degrees celsius is equal to: " + celsius.toFixed(2) + " °C"
 }
